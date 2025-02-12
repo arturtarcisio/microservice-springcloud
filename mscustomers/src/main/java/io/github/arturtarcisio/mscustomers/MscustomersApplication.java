21985b73e7126803e1fffd73ee5c7a6779811f26
arturtarcisio <arturtarcisio1@gmail.com>
2025-02-12 19:27:47 -0300

    Customers micro service created

---
+++ mscustomers/src/main/java/io/github/arturtarcisio/mscustomers/MscustomersApplication.java
@@ -0,0 +1,15 @@
+package io.github.arturtarcisio.mscustomers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MscustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscustomersApplication.class, args);
+	}
+
+}
---
+++ mscustomers/src/test/java/io/github/arturtarcisio/mscustomers/MscustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.arturtarcisio.mscustomers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
