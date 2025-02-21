8cbac559e0941edab0db5fe017eb1d4b843381fa
arturtarcisio <arturtarcisio1@gmail.com>
2025-02-20 21:55:53 -0300

    Testing load balancing with the gateway

--- mscustomers/src/main/java/io/github/arturtarcisio/mscustomers/controller/CustomerController.java
+++ mscustomers/src/main/java/io/github/arturtarcisio/mscustomers/controller/CustomerController.java
@@ -3,6 +3,7 @@ package io.github.arturtarcisio.mscustomers.controller;
 import io.github.arturtarcisio.mscustomers.domain.Customer;
 import io.github.arturtarcisio.mscustomers.dto.CustomerSaveRequest;
 import io.github.arturtarcisio.mscustomers.service.CustomerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -11,12 +12,19 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("customers")
+@Slf4j
 public class CustomerController {
 
     private final CustomerService customerService;
 
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
+    }
+
+    @GetMapping
+    public String status() {
+        log.info("obtendo status do microservice de cliente");
+        return "ok";
     }
 
     @PostMapping
