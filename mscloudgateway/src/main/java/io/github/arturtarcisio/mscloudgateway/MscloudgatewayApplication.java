8d1107c26133bb618d6711fe59fcababb8fa7b69
arturtarcisio <arturtarcisio1@gmail.com>
2025-03-27 18:26:54 -0300

    Updated gateway with cards microservice

--- mscloudgateway/src/main/java/io/github/arturtarcisio/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/src/main/java/io/github/arturtarcisio/mscloudgateway/MscloudgatewayApplication.java
@@ -22,7 +22,7 @@ public class MscloudgatewayApplication {
 		return routeLocatorBuilder
 				.routes()
 					.route(route -> route.path("/customers/**").uri("lb://mscustomers"))
-					.route(route -> route.path("/cards/**").uri("lb://cards"))
+					.route(route -> route.path("/cards/**").uri("lb://mscards"))
 				.build();
 	}
 
